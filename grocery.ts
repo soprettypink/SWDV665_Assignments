8e7beb251a44b779c43a80ce22846fa000f71eec
Sarah <46945965+soprettypink@users.noreply.github.com>
2021-03-13 22:39:41 -0500

    Module 1 Assignment

---
+++ grocery.ts
@@ -0,0 +1,20 @@
+interface Grocery {
+    item: string;
+    quantity: number;
+  }
+  
+  class GroceryList {
+    item: string;
+    quantity: number;
+  
+    constructor(item: string, qunatity: number) {
+      this.item = item;
+      this.quantity = this.quantity;
+    }
+  }
+  
+type ItemArray = Array<string>;
+const item = ['milk', 'bread', 'eggs', 'apples', 'carrots'];
+
+type QuantityArray = Array<number>;
+const quantity = [3, 1, 6, 5, 8];
